41e29de4377be89babf2efdc8cd2266312237a98
sumeet rohra <sumeetrohra@sumeets-MacBook-Pro.local>
2022-01-03 14:07:42 +0530

    feat: init

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    indent: ['error', 2],
+    'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'react/prop-types': 0,
+    '@typescript-eslint/no-empty-function': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+    'react/react-in-jsx-scope': 'off',
+  },
+};
